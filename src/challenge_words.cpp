5894dff78fb1999721f25f457684c3f94bd2938d
Jean Tampon <jean.tampon@gmail.com>
2019-05-10 07:15:35 +0200

    Add updated demo

--- src/challenge_words.cpp
+++ src/challenge_words.cpp
@@ -11,7 +11,7 @@ ChallengeWords::ChallengeWords(uint32_t width, uint32_t height)
 	, m_stats(width, 50.0f, 0.0f, 500.0f)
 	, m_input(800.0f, 120.0f, (width - 800.0f)*0.5f, 700)
 	, m_blur(width, height, 1.0f)
-	, m_duration(10.0f)
+	, m_duration(60.0f)
 	, m_timer(100.0f, 800.0f, 150.0f, m_duration)
 {
 	m_blur_texture.create(width, height);
